d7d32bfac189e5fb6c553d993ce9e50f9ef00a58
Yan Soares <yts.1995@gmail.com>
2020-09-09 22:15:38 -0300

    feat: pokejump working

--- script.js
+++ script.js
@@ -2,28 +2,51 @@ var character =
 document.getElementById("character");
 var block = 
 document.getElementById("block");
+
+var score = 0;
+var scoreId = document.getElementById("score");
+var tecla = 0;
+
+// document.addEventListener('keypress', function(event) {
+//     if (event.which === 32) {
+//         // ' ' is standard, 'Spacebar' was used by IE9 and Firefox < 37
+//         // e.preventDefault()
+//         tecla = event.which;
+
+//         console.log(tecla);
+//     }
+// })
+
+
+
+
 function jump() {
     if (character.classList != "animate") {
         character.classList.add("animate");
+        scoreId.textContent = 0;
     }
-        setTimeout(function(){
-            character.classList.remove("animate");
-    },500);
+    
+    setTimeout(function() {
+        character.classList.remove("animate");
+        scoreId.textContent = score;
+        score++;
+    }, 500);
 }
 
-var checkDead = setInterval(function() {
-        var characterTop = 
-        parseInt (window.getComputedStyle(character).getPropertyValue("top"));
-        var blockLeft = 
-        parseInt (window.getComputedStyle(block).getPropertyValue("left"));
-        
-        if(blockLeft < 20 && blockLeft > 0 && characterTop >= 130) {
-                block.style.animation = "none";
-                block.style.display = "none";
-                alert("YOU LOSE!")
-            }
 
+var checkDead = setInterval(function() {
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    
+    if (blockLeft < 20 && blockLeft > 0 && characterTop >= 130) {
+        block.style.animation = "none";
+        block.style.display = "none";
+        scoreId.textContent = 0;
+        alert("YOU LOSE!");
+        score = 0;
+    } else {
+        scoreId.textContent = score;
+    }
 }, 10);
-    
-   
-    
+
+
